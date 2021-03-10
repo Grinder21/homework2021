bba85dfe3396dea2eabdadba2dfe57246494a33c
Grinder21 <58875130+Grinder21@users.noreply.github.com>
2021-03-10 15:13:09 +0300

    Delete src directory

--- src/components/Navigator/Files/Age.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Age = (props) => {
-    return(
-    <div>{props.state}</div>
-    );
-}
-
-export default Age;
--- src/components/Navigator/Files/Group.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Group = (props) => {
-    return(
-    <div>{props.state}</div>
-    );
-}
-
-export default Group;
