0d0ac5e8bd6537d521d1ffa47d28fa14df01073b
Grinder21 <58875130+Grinder21@users.noreply.github.com>
2021-03-10 14:35:54 +0300

    Add files via upload

---
+++ src/components/Dashboard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import StyleDashboard from "./Dashboard.module.css";
+class Dashboard extends React.Component {
+  state = {
+    users: [],
+    error: "",
+    studentId: "",
+  };
+  handleClick = () => {
+    const { handleLoginClick } = this.props;
+    handleLoginClick(false);
+  };
+
+  // getStudentId = (student) => {
+  //   this.setState({ studentId: student });
+  //   console.log("Я с дашборда" + student);
+  // };
+
+  render() {
+    return (
+      <div className={StyleDashboard.dashboard}>
+        <div>{this.props.title}</div>
+        <button
+          className={StyleDashboard.mainButton}
+          onClick={this.handleClick}
+        >
+          Выйти
+        </button>
+      </div>
+    );
+  }
+}
+export default Dashboard;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import StyleLoginForm from "./LoginForm.module.css";
+
+class LoginForm extends React.Component {
+  handleClick = () => {
+    const { handleLoginClick } = this.props;
+    handleLoginClick(true);
+  };
+
+  render() {
+    // const { onButtonClick } = this.props;
+
+    return (
+      <div className={StyleLoginForm.mainDiv}>
+        <div className={StyleLoginForm.childrenDiv}>
+          <input className={StyleLoginForm.mainInput} type="text" required />
+          <button className={StyleLoginForm.loginButton} onClick={this.handleClick}>Войти</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
