7fc895832f744790e1261af1ddd12cc5ac2c5458
Grinder21 <58875130+Grinder21@users.noreply.github.com>
2021-03-12 22:10:23 +0300

    Delete src directory

--- src/components/Navigator/Files/Name.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Name = (props) => {
-    return(
-    <div>{props.state}</div>
-    );
-}
-
-export default Name;
--- src/components/store/index.js
+++
@@ -1,6 +0,0 @@
-import MainStore from "./MainStore";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  MainStore: new MainStore(),
-};
